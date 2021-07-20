d8eebf2612bf12c00f0c7f7aa9b1700f8af63ddb
shadymohammedd <shadymohammed970@gmail.com>
2021-07-20 22:36:49 +0200

    created the main project

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void fun(int *p1, int *p2)
+{
+
+
+    printf("x = %d\ny = %d", *p1, *p2);
+}
+
+
+int main()
+{
+    int x = 7;
+    int y = 8;
+
+    fun(&x, &y);
+
+    return 0;
+}
